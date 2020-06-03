d15e1db940b826e0a606fa6899aa14aa2f3ffa78
Isa <isa1perezpe@gmail.com>
2020-06-02 21:52:54 -0500

    Titulo de Controlador.71e6daa8bc022ee5e612fe66af61755e66920806

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace telebachillerato
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js", "~/Scripts/Content/js/bootstrap.min.js", "~/Scripts/Content/js/jquery-1.11.2.min.js", "~/Scripts/Content/js/main.js", "~/Scripts/Content/js/modernizr.js",
+                      "~/Scripts/Content/js/sb-admin-2.js", "~/Scripts/Content/js/sb-admin-2.min.js", "~/Scripts/Content/js/demo/chart-area-demo.js", "~/Scripts/Content/js/demo/chart-bar-demo.js",
+                      "~/Scripts/Content/js/demo/chart-pie-demo.js", "~/Scripts/Content/js/demo/datatables-demo.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css", "~/Content/css/bootstrap.min.css", "~/Content/css/font-awesome.min.css", "~/Content/css/normalize.css", "~/Content/css/sb-admin-2.css", "~/Content/css/sb-admin-2.min.css",
+                      "~/Content/css/style.css"));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="telebachillerato.MvcApplication" Language="C#" %>
